d26266a60248ce20abf2ec1ffc0aad4c1dce963b
fercol <colchero@imada.sdu.dk>
2025-05-21 13:39:23 +0200

    small changes to Example2

--- devel/Example2.R
+++ devel/Example2.R
@@ -2,22 +2,26 @@ library(CreateRproj)
 
 # Main directory:
 mainDir <- "~/FERNANDO/PROJECTS/1.ACTIVE/BaboonEarlyAdversity/analysis/"
-mainDir <- "~/FERNANDO/PROJECTS/1.ACTIVE/cancer/"
+mainDir <- "~/FERNANDO/PROJECTS/1.ACTIVE/ContraceptionOnMort/"
+mainDir <- "/Users/fernando_colchero/Dropbox/00.Personal/FERNANDO/PROJECTS/1.ACTIVE/DormouseMort/"
+mainDir <- "~/Nextcloud/Shared/Projects/"
 
 # set wd:
 setwd(mainDir)
 
 # project name:
-projName <- "cancerAgeing"
+projName <- "SlowFastMor"
 
 # Create project:
 if (!projName %in% list.files()) {
   CreateRproj(projName = projName, mainDir = mainDir, git = TRUE, 
               RstProj = TRUE)
 }
+
+
 # Script name:
-scriptName <- "prepMorbMortdata"
+scriptName <- "pHackingBiasExplore"
 
 # Create R script:
-CreateRscript(file = sprintf("cancerAgeing/02code/%s.R", scriptName), 
-              sections = c("FUNCTIONS", "DATA PREP."))
+CreateRscript(file = sprintf("%s/02code/%s.R", projName, scriptName), 
+              sections = c("FUNCTIONS", "DATA PREP.", "ANALYSES"))
